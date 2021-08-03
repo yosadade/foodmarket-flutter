8aa97a90bf1a22b08d669a01c1d2f739f93d8b63
Yosada Dede <masyosad@gmail.com>
2021-08-03 13:56:42 +0700

    Main Page

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:foodmarket/shared/shared.dart';
+import 'package:foodmarket/ui/widgets/widgets.dart';
 import 'package:get/state_manager.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supercharged/supercharged.dart';
@@ -11,4 +12,5 @@ part 'general_page.dart';
 part 'sign_in_page.dart';
 part 'sign_up_page.dart';
 part 'address_page.dart';
+part 'main_page.dart';
 
